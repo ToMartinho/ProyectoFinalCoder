1e40bef197b6d4d1de0fb1d98c3ba0f83ea8d1d9
ToMartinho <tome.martinho@gmail.com>
2022-08-26 03:15:13 -0300

    se arregla error en el forms

--- ProyectoSTT/app_STT/forms.py
+++ ProyectoSTT/app_STT/forms.py
@@ -1,7 +1,7 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm,User
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.contrib.auth import
+
 
 from app_STT.models import avatar
 
